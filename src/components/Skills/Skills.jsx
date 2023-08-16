1e94b5f7b8a0d93f1c3f9274e1fef8ef9a89d2c1
ivanlima096 <ivanlima096@gmail.com>
2023-08-16 18:02:16 -0300

    skills built but not straight

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,20 +1,78 @@
 import tech from '../../assets/tech.png';
-import { AiFillHtml5 } from 'react-icons/ai';
+import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoMongodb, BiLogoTailwindCss, BiLogoNodejs, BiLogoGit, BiLogoReact } from 'react-icons/bi';
 import styles from './Skills.css';
+import { useState } from 'react';
 
 export default function Skills() {
+  const [skills, setSkills] = useState([
+    {
+      name: "HTML",
+      icon: BiLogoHtml5,
+      backgroundColor: "#FFF",
+      color: "#E44F26"
+    },
+    {
+      name: "CSS",
+      icon: BiLogoCss3,
+      backgroundColor: "#FFF",
+      color: "#1572B6"
+    },
+
+    {
+      name: "Javascript",
+      icon: BiLogoJavascript,
+      backgroundColor: "#F7DF1E",
+      color: "#000"
+    },
+    {
+      name: "MongoDB",
+      icon: BiLogoMongodb,
+      backgroundColor: "#84BD48",
+      color: "#FFF"
+    },
+    {
+      name: "Tailwind",
+      icon: BiLogoTailwindCss,
+      backgroundColor: "#FFF",
+      color: "#1572B6"
+    },
+    {
+      name: "Node",
+      icon: BiLogoNodejs,
+      backgroundColor: "#84BD48",
+      color: "#FFF"
+    },
+    {
+      name: "Git",
+      icon: BiLogoGit,
+      backgroundColor: "#FFFFFF",
+      color: "#F05033"
+    },
+    {
+      name: "React",
+      icon: BiLogoReact,
+      backgroundColor: "#FFF",
+      color: "#1572B6"
+    },
+  ]);
   return (
     <section id="skills" className='flex flex-col'>
-      <h1 className='mx-auto mb-4 font-bold text-5xl'>SKILLS</h1>
+      <h1 className='mx-auto font-bold text-5xl'>SKILLS</h1>
 
       <div className="h-[93vh] flex items-center justify-center relative">
         <div className="planet-orbit">
-          <div id="html" className="planet shadow-lg flex flex-col">
-            <span className='font-bold'>HTML</span>
-            <AiFillHtml5 size={120} />
-          </div>
-        </div>
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="planet shadow-lg flex flex-col "
+              style={{ backgroundColor: `${skill.backgroundColor}`, color: `${skill.color}` }}
 
+            >
+              <span className='font-bold'>{skill.name}</span>
+              <skill.icon size={120} />
+            </div>
+          ))}
+        </div>
         <img src={tech} alt="tech" className="centered-image md:w-[18rem] sm:w-[12rem] w-[8rem]" />
       </div>
     </section>
