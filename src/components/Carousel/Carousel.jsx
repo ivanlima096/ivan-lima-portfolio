3e21214e768284888726e07ae217de61ef8d55c5
ivanlima096 <ivanlima096@gmail.com>
2023-08-21 20:25:33 -0300

    projects add

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,15 @@
 import { Carousel } from "@material-tailwind/react";
 import teste from "../../assets/teste.png";
-import projects1 from "../../assets/projects-1.png";
-import projects3 from "../../assets/projects-3.png";
+import projects1 from "../../assets/Project 1/projects-1.png";
+import projects1video from "../../assets/Project 1/project1video.mp4";
+import projects2 from "../../assets/Project 2/projects-2.png";
+import projects2video from "../../assets/Project 2/project2video.mp4";
+import projects3 from "../../assets/Project 3/projects-3.png";
+import projects3video from "../../assets/Project 3/project3video.mp4";
+import projects4 from "../../assets/Project 4/projects-4.png";
+import projects4video from "../../assets/Project 4/project4video.mp4";
+import projects5 from "../../assets/Project 5/projects-5.png";
+import projects5video from "../../assets/Project 5/project5video.mp4";
 import Button from "../Button/Button";
 import { BsEyeFill, BsArrowUpRightCircleFill } from "react-icons/bs";
 import styles from "./Carousel.css";
@@ -11,27 +19,48 @@ import Modal from "../Modal/Modal";
 export const projects = [
   {
     name: "Tailwind Site",
-    url: projects1,
-    demoURL: "https://google.com",
-    githubURL: "https://github.com/ivanlima096",
+    photo: projects1,
+    site: "https://silver-salmiakki-1bb6ea.netlify.app/",
+    demoURL: projects1video,
+    githubURL: "https://github.com/ivanlima096/site-tailwind",
     description: "Projeto React criado com o objetivo de praticar habilidades de desenvolvimento web utilizando Tailwind CSS, componentização e a biblioteca react-typed. O site desenvolvido é uma aplicação interativa que demonstra a integração dessas tecnologias.",
     technologies: "Javascript, React, CSS, Tailwind, React-typed"
   },
   {
-    name: "Segundo Site",
-    url: teste,
-    demoURL: "https://youtube.com",
-    githubURL: "https://github.com/ivanlima096",
-    description: "Projeto React criado com o objetivo de praticar habilidades de desenvolvimento web utilizando Tailwind CSS, componentização e a biblioteca react-typed. O site desenvolvido é uma aplicação interativa que demonstra a integração dessas tecnologias.",
-    technologies: "Javascript, React, CSS, Tailwind, React-typed"
+    name: "Mercado e carrinho com React",
+    photo: projects2,
+    site: "https://64e3e97e71082716e70c124a--effulgent-kringle-c9dc10.netlify.app/",
+    demoURL: projects2video,
+    githubURL: "https://github.com/ivanlima096/react-market",
+    description: "Projeto React para praticar o desenvolvimento web com a API do Mercado Livre. Uma loja de produtos onde os usuários podem, Visualizar produtos disponíveis, Pesquisar produtos específicos, Adicionar e remover itens do carrinho de compras, Ver o total e quantidade de itens no carrinho.",
+    technologies: "Javascript, React, CSS"
   },
   {
-    name: "Terceiro Site",
-    url: projects3,
-    demoURL: "https://facebook.com",
-    githubURL: "https://github.com/ivanlima096",
-    description: "Projeto React criado com o objetivo de praticar habilidades de desenvolvimento web utilizando Tailwind CSS, componentização e a biblioteca react-typed. O site desenvolvido é uma aplicação interativa que demonstra a integração dessas tecnologias.",
-    technologies: "Javascript, React, CSS, Tailwind, React-typed"
+    name: "Galeria de Times",
+    photo: projects3,
+    site: "https://64e3ee224f99a21b3aebb573--vermillion-choux-a71b6d.netlify.app/",
+    demoURL: projects3video,
+    githubURL: "https://github.com/ivanlima096/team-gallery",
+    description: "Projeto pessoal feito em React e react router dom. Utilização de vários hooks, inclusive um feito personalizado para otimização de produtividade. Utilização de diferentes rotas com createBrowserRouter e Link to. Props e useContext para transferência de informações de propriedades por todo o arquivo.",
+    technologies: "Javascript, React, CSS, Router"
+  },
+  {
+    name: "Apostas em Jockey",
+    photo: projects4,
+    site: "https://lighthearted-kitsune-e36d17.netlify.app/",
+    demoURL: projects4video,
+    githubURL: "https://github.com/ivanlima096/jockey-bets",
+    description: "Projeto feito para praticar Javascript Vanilla com aplicação de funções e lógica de programação, baseado em Exercício do Livro 'Lógica de Programação e Algoritmos com Javascript' da editora Novatec.",
+    technologies: "HTML, CSS, Bootstrap e JS"
+  },
+  {
+    name: "Biblioteca de Filmes",
+    photo: projects5,
+    site: "https://64e3f316350b1d1dff77223c--bright-melomakarona-a63c51.netlify.app/",
+    demoURL: projects5video,
+    githubURL: "https://github.com/ivanlima096/movies-lib-tmdb-api",
+    description: "Projeto feito para praticar React com criação de components, manipulações de dados com dados dinâmicos, estilização com CSS e uso de API da TMDB a partir de vídeo Matheus Battisti - Hora de Codar.",
+    technologies: "CSS, Bootstrap e JS, React e APIs"
   },
 ];
 
@@ -48,21 +77,21 @@ export default function CarouselCustomNavigation() {
     <section className="carousel">
       <Carousel
         transition={{ duration: 1 }}
-        className="rounded-xl mb-10 w-[82vw] lg:w-[80vw] bg-[#FFF] border-2 border-black md:w-[100%]"
+        className="rounded-xl mb-10 w-[82vw] lg:w-[80vw] bg-[#FFF] border-2 border-black md:w-[100%] shadow-2xl"
       >
         {projects.map((project, index) => (
           <div key={index} className="slide-container">
             <div
               className="h-auto w-[100%] aspect-video md:h-[65vh] md:aspect-auto flex flex-col md:flex-row gap-5 font-bold text-lg overflow-hidden">
               <img
-                src={project.url}
+                src={project.photo}
                 alt={project.name}
-                className="bg-black h-full w-full object-contain"
+                className="bg-black h-full w-full object-contain object-center"
               />
             </div>
             <div
               className='border-t-2 border-black bg-white flex flex-col items-center pt-8 pb-2 gap-6 font-bold text-lg justify-center md:flex-row md:justify-around md:gap-0'>
-              <Button text={`Ver Site`} icon={<BsArrowUpRightCircleFill />} url={project.demoURL} />
+              <Button text={`Ver Site`} icon={<BsArrowUpRightCircleFill />} url={project.site} />
               <Button text={`Ver Prévia`} icon={<BsEyeFill />} handleFunction={() => handleModal(index)} />
             </div>
           </div>
@@ -83,6 +112,9 @@ export default function CarouselCustomNavigation() {
           projectGithub={project.githubURL}
           projectDescription={project.description}
           projectTech={project.technologies}
+          projectVideo={project.demoURL}
+          projectSite={project.site}
+
         />
       ))}
     </section>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,19 @@
 import Button from '../Button/Button';
 import { BiXCircle } from 'react-icons/bi';
-import { BsEyeFill, BsArrowUpRightCircleFill } from "react-icons/bs";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 import { BiLogoGithub } from "react-icons/bi";
 
-
-
-export default function Modal({ isOpen, setIsOpenModal, projectName, projectUrl, projectGithub, projectDescription, projectTech }) {
+export default function Modal({
+  isOpen,
+  setIsOpenModal,
+  projectName,
+  projectUrl,
+  projectGithub,
+  projectDescription,
+  projectTech,
+  projectVideo,
+  projectSite
+}) {
   const handleContentClick = (event) => {
     event.stopPropagation();
   }
@@ -13,35 +21,43 @@ export default function Modal({ isOpen, setIsOpenModal, projectName, projectUrl,
   if (isOpen) {
     return (
       <div
-        className='modalBackground py-2 md:p-[10rem] fixed top-0 left-0 bottom-0 right-0 bg-[#000000b3] z-50 flex items-center justify-center'
+        className=' py-2 md:p-[10rem] fixed top-0 left-0 bottom-0 right-0 bg-[#000000b3] z-50 flex items-center justify-center'
         onClick={setIsOpenModal}>
         <div
-          className='bg-white border-gray-600 border-2 flex flex-col rounded-2xl p-5 top-0 left-0 h-[60vh] w-[90vw] md:h-[78vh] md:w-[90vw] lg:h-[95vh] lg:w-[95vw] max-w-[95vw] aspect-video'
-          onClick={handleContentClick}>
-          <div className='bg-black rounded-xl flex flex-col items-center '>
-
-            <div className=' flex justify-between px-4 py-1 w-[100%]'>
-              <h1 className=' text-2xl md:text-4xl uppercase font-bold py-2'>{projectName}</h1>
-              <button
-                className='text-xl md:text-3xl'
-                onClick={setIsOpenModal}><BiXCircle /></button>
+          className='relative rounded-3xl p-5 h-[70vh] w-[90vw] md:h-[78vh] md:w-[90vw] lg:h-[95vh] lg:w-[95vw] max-w-[95vw] aspect-video'
+          onClick={handleContentClick}
+        >
+          <video
+            autoPlay
+            muted
+            loop
+            src={projectVideo}
+            alt={projectName}
+            className='absolute top-0 left-0 w-full h-full object-cover z-0 rounded-3xl'
+          />
+          <div className='absolute top-0 left-0 w-full h-full z-1 flex flex-col items-center justify-end'>
+            <div className='bg-[#000000b3] rounded-xl p-5 shadow-md'>
+              <div className='flex justify-between'>
+                <h1 className='text-2xl md:text-4xl uppercase font-bold py-2'>{projectName}</h1>
+                <button className='text-xl md:text-3xl' onClick={setIsOpenModal}>
+                  <BiXCircle />
+                </button>
+              </div>
+              <div className='text-[0.5rem] md:text-[0.8rem] lg:text-lg'>
+                <h4>Descrição</h4>
+                <p className='pb-2'>{projectDescription}</p>
+                <h4>Tecnologias Utilizadas</h4>
+                <p>{projectTech}</p>
+              </div>
+              <div className='flex justify-center mt-4 gap-5 font-bold'>
+                <Button className='button mr-4' text={`Ver Site`} icon={<BsArrowUpRightCircleFill />} url={projectSite} />
+                <Button text={`Github`} icon={<BiLogoGithub />} url={projectGithub} />
+              </div>
             </div>
-            <div className='flex flex-col text-[0.5rem] md:text-[0.8rem] lg:text-lg p-1'>
-              <img src={projectUrl} alt={projectName} className='rounded-2xl p-1 md:py-4 h-[22vh] md:h-[40vh] lg:h-[48vh] aspect-video object-cover' />
-              <h4>Descrição</h4>
-              <p className='pb-2'>{projectDescription}</p>
-              <h4>Tecnologias Utilizadas</h4>
-              <p>{projectTech}</p>
-            </div>
-          </div>
-          <div
-            className='bg-white flex flex-col items-center pt-2 pb-2 gap-2 font-bold text-sm md:text-xl justify-center md:flex-row md:justify-around md:gap-0'>
-            <Button className="button" text={`Ver Site`} icon={<BsArrowUpRightCircleFill />} url={projectUrl} />
-            <Button text={`Github`} icon={<BiLogoGithub />} url={projectGithub} />
           </div>
         </div>
-      </div >
-    )
+      </div>
+    );
   }
-  return null
+  return null;
 }
